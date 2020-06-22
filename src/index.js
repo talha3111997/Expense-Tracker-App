67bbe9dee8644e68ffaeacc36d9b0291a0dcf56d
talha3111997 <talha3111997@gmail.com>
2020-06-22 18:14:43 +0500

    remove unused vars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Income from './Example'
 ReactDOM.render(
